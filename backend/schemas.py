47500b1cff2984d050ff81ab7b96a0c384b4e63b
Adriane <adrianebernardo.study@gmail.com>
2026-09-10 18:47:38 -0300

    chore: finaliza configuração do backend

---
+++ backend/schemas.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List
+
+
+class Product(BaseModel):
+
+    name: str
+
+    description: str
+
+    price: float
+
+    stock: int
+
+    image_url: Optional[str] = None
+
+    image_product: Optional[str] = None
+
+    images: Optional[List[str]] = Field(default_factory=list)
+
+    sizes: Optional[List[str]] = Field(default_factory=list)
+
+    category: Optional[str] = None
+
+
+class ProductUpdate(BaseModel):
+
+    name: Optional[str] = None
+
+    description: Optional[str] = None
+
+    price: Optional[float] = None
+
+    stock: Optional[int] = None
+
+    image_url: Optional[str] = None
+
+    image_product: Optional[str] = None
+
+    images: Optional[List[str]] = None
+
+    sizes: Optional[List[str]] = None
+
+    category: Optional[str] = None
